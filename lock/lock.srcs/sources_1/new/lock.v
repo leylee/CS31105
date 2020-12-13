acdfa230cde8e773429143217c54fc33ad2f1d92
leylee <leylee2001@gmail.com>
2020-12-14 00:21:22 +0800

    finish

--- lock/lock.srcs/sources_1/new/lock.v
+++ lock/lock.srcs/sources_1/new/lock.v
@@ -40,6 +40,9 @@ module lock(
     parameter THREE_SEC_TICKS = 300000000;
     parameter PASSWORD = 16'h2077;
 
+    reg [15:0] password;
+    reg [7:0] error_times;
+
     // 时钟计数器
     reg[31:0] ticks;
     // 判断当前能否接收按键
@@ -53,7 +56,7 @@ module lock(
     // 密码存储缓冲区
     reg [3:0] pwd[0:3];
     // 密码下一位的位置
-    reg [2:0] status;
+    reg [3:0] status;
     // 密码显示数据缓冲区
     reg [3:0] pwdbuf[0:3];
     // 提示信息显示数据缓冲区
@@ -87,7 +90,7 @@ module lock(
 
     // 舵机动作
     wire enable_servo;
-    assign enable_servo = status == 5;
+    assign enable_servo = (status == 5) | ((status >= 8) & (status <= 4'hC));
     servo unlock_servo(
         .clk(clk),
         .enable(enable_servo),
@@ -105,9 +108,9 @@ module lock(
     assign beep = red;
 
     // rgb 灯
-    assign red = (status == 3'h6) | (status == 3'h7);
-    assign blue = (status == 3'h0) | (status == 3'h1) | (status == 3'h2) | (status == 3'h3);
-    assign green = (status == 3'h5);
+    assign red = (status == 4'h6) | (status == 4'h7) | (status == 4'hD);
+    assign blue = (status <= 4'h3);
+    assign green = (status == 4'h5) | ((status >= 8) & (status <= 4'hC));
 
     always @(negedge clk, negedge rst) begin
         if (~rst) begin
@@ -117,13 +120,15 @@ module lock(
             {msgbuf[0], msgbuf[1], msgbuf[2], msgbuf[3]} <= 16'hFFFF;
             softrst <= 1'b1;
             readable <= 1'b1;
+            password <= PASSWORD;
+            error_times <= 8'h0;
         end
         else if (~softrst) begin
             softrst <= 1'b1;
         end
         else begin
             case (status)
-                3'h0: begin
+                4'h0: begin
                     if (has_value) begin
                         readable <= 1'b0;
                         pwd[0] <= value;
@@ -132,12 +137,13 @@ module lock(
                         pwdbuf[1] <= 4'hA;
                     end
                 end
-                3'h1, 3'h2: begin
+                4'h1, 4'h2: begin
                     if (ticks > FIVE_SEC_TICKS) begin
                         {msgbuf[0], msgbuf[1], msgbuf[2], msgbuf[3]} <= 16'hD01F;
                         ticks <= 32'h0;
                         {pwdbuf[0], pwdbuf[1], pwdbuf[2], pwdbuf[3]} <= 16'hFFFF;
-                        status <= 3'h6;
+                        status <= 4'h6;
+                        error_times <= error_times + 8'h1;
                         pwdbuf[0] <= 4'hA;
                         softrst <= 1'b1;
                         readable <= 1'b1;
@@ -149,8 +155,8 @@ module lock(
                                 readable <= 1'b0;
                                 pwd[status] <= value;
                                 pwdbuf[status] <= value;
-                                status <= status + 3'h1;
-                                pwdbuf[status + 3'h1] <= 4'hA;
+                                status <= status + 4'h1;
+                                pwdbuf[status + 4'h1] <= 4'hA;
                             end
                         end
                         else begin
@@ -160,12 +166,13 @@ module lock(
                         end
                     end
                 end
-                3'h3: begin
+                4'h3: begin
                     if (ticks > FIVE_SEC_TICKS) begin
                         {msgbuf[0], msgbuf[1], msgbuf[2], msgbuf[3]} <= 16'hD01F;
                         ticks <= 32'h0;
                         {pwdbuf[0], pwdbuf[1], pwdbuf[2], pwdbuf[3]} <= 16'hFFFF;
-                        status <= 3'h6;
+                        status <= 4'h6;
+                        error_times <= error_times + 8'h1;
                         pwdbuf[0] <= 4'hA;
                         softrst <= 1'b1;
                         readable <= 1'b1;
@@ -188,31 +195,101 @@ module lock(
                         end
                     end
                 end
-                3'h4: begin
-                    if ({pwd[0], pwd[1], pwd[2], pwd[3]} == PASSWORD) begin
+                4'h4: begin
+                    if ({pwd[0], pwd[1], pwd[2], pwd[3]} == password) begin
                         {msgbuf[0], msgbuf[1], msgbuf[2], msgbuf[3]} <= 16'hBCDE;
                         status <= 5;
+                        error_times <= 8'h0;
                     end
                     else begin
                         {msgbuf[0], msgbuf[1], msgbuf[2], msgbuf[3]} <= 16'hD02F;
                         status <= 7;
+                        error_times <= error_times + 8'h1;
                     end
                 end
-                3'h5, 3'h6, 3'h7: begin
+                4'h5: begin
                     if (ticks < THREE_SEC_TICKS) begin
                         ticks <= ticks + 1;
+                        if (readable) begin
+                            if (has_value) begin
+                                readable <= 1'b0;
+                                {pwdbuf[0], pwdbuf[1], pwdbuf[2], pwdbuf[3]} <= 16'hAFFF;
+                                status <= 4'h8;
+                            end
+                        end
+                        else begin
+                            if (~has_value) begin
+                                readable <= 1'b1;
+                            end
+                        end
                     end
                     else begin
                         ticks <= 32'h0;
                         {pwdbuf[0], pwdbuf[1], pwdbuf[2], pwdbuf[3]} = 16'hAFFF;
-                        status <= 3'h0;
+                        status <= 4'h0;
                         {msgbuf[0], msgbuf[1], msgbuf[2], msgbuf[3]} = 16'hFFFF;
                         softrst <= 1'b1;
                         readable <= 1'b1;
                     end
                 end
+                4'h6, 4'h7: begin
+                    if (error_times == 8'h3) begin
+                        status <= 4'hD;
+                    end
+                    else if (ticks < THREE_SEC_TICKS) begin
+                        ticks <= ticks + 1;
+                    end
+                    else begin
+                        ticks <= 32'h0;
+                        {pwdbuf[0], pwdbuf[1], pwdbuf[2], pwdbuf[3]} = 16'hAFFF;
+                        status <= 4'h0;
+                        {msgbuf[0], msgbuf[1], msgbuf[2], msgbuf[3]} = 16'hFFFF;
+                        softrst <= 1'b1;
+                        readable <= 1'b1;
+                    end
+                end
+                4'h8, 4'h9, 4'hA: begin
+                    if (readable) begin
+                        if (has_value) begin
+                            readable <= 1'b0;
+                            pwd[status - 4'h8] <= value;
+                            pwdbuf[status - 4'h8] <= value;
+                            status <= status + 4'h1;
+                            pwdbuf[status - 4'h7] <= 4'hA;
+                        end
+                    end
+                    else begin
+                        if (~has_value) begin
+                            readable <= 1'b1;
+                        end
+                    end
+                end
+                4'hB: begin
+                    if (readable) begin
+                        if (has_value) begin
+                            readable <= 1'b0;
+                            pwd[status - 4'h8] <= value;
+                            pwdbuf[status - 4'h8] <= value;
+                            status <= 4'hC;
+                        end
+                    end
+                    else begin
+                        if (~has_value) begin
+                            readable <= 1'b1;
+                        end
+                    end
+                end
+                4'hC: begin
+                    password = {pwd[0], pwd[1], pwd[2], pwd[3]};
+                    status <= 4'h5;
+                    ticks <= 32'd0;
+                end
+                4'hD: begin
+                    {msgbuf[0], msgbuf[1], msgbuf[2], msgbuf[3]} <= 16'hD03F;
+                    status <= 4'hD;
+                end
                 default: 
-                    status <= 3'h0;
+                    status <= 4'h0;
             endcase
         end
     end
